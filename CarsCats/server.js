ff5bc3185eca33ea2fecb9ffb416253eb9bc8d39
Javier Gonzalez <javig56@gmail.com>
2018-05-01 16:33:28 -0700

    EJS cars and cats upload

--- CarsCats/server.js
+++ CarsCats/server.js
@@ -4,6 +4,18 @@ var app = express();
 
 app.use(express.static(__dirname + "/static"));
 
+app.set('views', __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.listen(8000, function(){
         console.log("listening on port 8000")
+})
+app.get("/cars", function(request, response){
+        response.render('cars');
+})
+app.get("/cats", function(request, response){
+        response.render('cats');
+})
+app.get("/cars/new", function(request, response){
+        response.render('new');
 })
